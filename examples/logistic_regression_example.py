ca2d90195a2887eb4fc4ea22446d7f3c93bca3c5
robprob <liamrhaney@gmail.com>
2025-07-03 11:44:45 -0700

    Logistic Regression: Added static seeding to example script

--- examples/logistic_regression_example.py
+++ examples/logistic_regression_example.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression as ScikitLogisticRegression
 
 data = sklearn.datasets.make_classification(25, 10, random_state=115)
 X, y = data[0], data[1]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=115)
 
 models = {
     "Gradient Descent": LogisticRegression(),
