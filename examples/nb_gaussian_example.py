306efac0d5c589ce096efc374d1a77db367b9f06
robprob <liamrhaney@gmail.com>
2025-07-04 09:13:54 -0700

    Naive Bayes Gaussian Classification, Added/Updated NB example scripts

--- examples/nb_gaussian_example.py
+++ examples/nb_gaussian_example.py
@@ -2,17 +2,15 @@ import sklearn.datasets
 import numpy as np
 from rasapy.utils.preprocessing import train_test_split
 from rasapy.bayesian.nb_classification import NaiveBayesClassification
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
 
 data = sklearn.datasets.make_classification(n_samples=100, n_features=10, n_informative=4, class_sep=2.0, random_state=115)
 X, y = data[0], data[1]
-X = np.array(X >= np.mean(X, axis=0), dtype=int)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=115)
 
 models = {
-    "Multinomial Naive Bayes Classification": NaiveBayesClassification(distribution='multinomial'),
-    "Alpha = 0.0 (no smoothing)": NaiveBayesClassification(alpha=0.0),
-    "Sklearn MultinomialNB (log smoothing)": MultinomialNB(alpha=0.0)
+    "Gaussian Naive Bayes Classification": NaiveBayesClassification(distribution='gaussian'),
+    "Sklearn GaussianNB": GaussianNB()
 }
 
 for name, model in models.items():
--- rasapy/bayesian/nb_classification.py
+++ rasapy/bayesian/nb_classification.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 
 from rasapy.metrics.classification import accuracy
 
@@ -9,7 +10,7 @@ class NaiveBayesClassification:
     """
     def __init__(self, distribution='multinomial', alpha=1.0):
         # Validate distribution input
-        if distribution in {'multinomial'}:
+        if distribution in {'multinomial', 'gaussian'}:
             self.distribution = distribution
         else:
             raise ValueError(f"Invalid distribution: {distribution}")
@@ -18,8 +19,14 @@ class NaiveBayesClassification:
         
         self.classes = None
         self.priors = None
+        
+        # Multinomial
         self.class_feature_counts = None
         self.class_feature_freq = None
+        
+        # Gaussian
+        self.means = None
+        self.vars = None
         
     def fit(self, X_train, y_train):
         """
@@ -34,21 +41,23 @@ class NaiveBayesClassification:
         # Delegate to distribution-specific model fit function
         if self.distribution == 'multinomial':
             self.fit_multinomial(X_train, y_train)
+        elif self.distribution == 'gaussian':
+            self.fit_gaussian(X_train, y_train)
     
     def fit_multinomial(self, X_train, y_train):
         """
         Fit multinomial data to the Naive Bayes model.
         """
-        num_features = X_train.shape[1]
+        num_classes, num_features = len(self.classes), X_train.shape[1]
         
         # Calculate total count of feature observations by class label
-        class_feature_counts = np.zeros((len(self.classes), num_features), dtype=int)
+        class_feature_counts = np.zeros((num_classes, num_features), dtype=int)
         for i, c in enumerate(self.classes):
             # Parse training entries where class label equals current class, sum along features axis (columns)
             class_feature_counts[i] = X_train[y_train == c].sum(axis=0)
         
         # Calculate frequency for each feature as a result of observed counts
-        class_feature_freq = np.zeros((len(self.classes), X_train.shape[1]), dtype=float)
+        class_feature_freq = np.zeros((num_classes, num_features), dtype=float)
         for i, c in enumerate(self.classes):
             # Perform Laplace smoothing
             # If feature is not seen in any a specific class outputs, it otherwise leads to 0 probability when frequencies are multiplied together
@@ -57,11 +66,42 @@ class NaiveBayesClassification:
         # Assign class feature counts to the model
         self.class_feature_freq = class_feature_freq
     
+    def fit_gaussian(self, X_train, y_train):
+        """
+        Fit continuous Gaussian data to the Naive Bayes model.
+        """
+        num_classes, num_features = len(self.classes), X_train.shape[1]
+        
+        # Calculate class-specific mean and variance for each feature
+        means = np.zeros((num_classes, num_features), dtype=float)
+        vars = np.zeros((num_classes, num_features), dtype=float)
+        
+        for i, c in enumerate(self.classes):
+            X_class = X_train[y_train == c]
+            means[i] = X_class.mean(axis=0)
+            vars[i] = np.maximum(np.var(X_class, axis=0), 1e-9) # prevent log(0) later on
+        
+        # Assign to model
+        self.means = means
+        self.vars = vars
+    
     def predict_proba(self, X):
         """
         Make class probability predictions based on the priors observed in the training data.
         """
-        return (X @ self.class_feature_freq.T) # Matrix of size (X.shape[0], num_classes)
+        num_classes, num_features = len(self.classes), X.shape[1]
+        
+        if self.distribution == 'multinomial':
+            y_proba = X @ self.class_feature_freq.T # Matrix of size (X.shape[0], num_classes)
+        elif self.distribution == 'gaussian':
+            y_proba = np.empty((X.shape[0], num_classes), dtype=float)
+            for i in range(len(self.classes)):
+                # Calculate log of Probability Density Function for a Gaussian distribution
+                log_likelihoods = stats.norm.logpdf(X, loc=self.means[i], scale=np.sqrt(self.vars[i])) # takes STD instead of variance
+                # Logarithmic product rule allows adding log likelihoods instead of multiplying raw likelihoods (risk of numerical instability/underflow)
+                y_proba[:, i] = log_likelihoods.sum(axis=1)
+        
+        return y_proba
         
     def predict(self, X):
         """
